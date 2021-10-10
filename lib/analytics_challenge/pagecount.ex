4996cd1ea2da0fdec7ce7cdf69775f82b4b5c684
Coit Scharringhausen <coit@simpli.fi>
2021-10-09 22:33:26 -0500

    Format cleanup

--- lib/analytics_challenge/pagecount.ex
+++ lib/analytics_challenge/pagecount.ex
@@ -8,21 +8,23 @@ defmodule AnalyticsChallenge.Pagecount do
   @type t :: %__MODULE__{}
 
   schema "pagecounts" do
-    field :language_code
-    field :page_name
-    field :view_count, :integer
-    field :year, :integer
-    field :month, :integer
-    field :day, :integer
+    field(:language_code)
+    field(:page_name)
+    field(:view_count, :integer)
+    field(:year, :integer)
+    field(:month, :integer)
+    field(:day, :integer)
   end
 
   def changeset(pagecount, attrs) do
     pagecount
     |> cast(attrs, [:language_code, :page_name, :view_count, :year, :month, :day])
     |> validate_required([:language_code, :page_name, :view_count])
-    |> validate_length(:language_code, min: 2, max: 3) # ISO 639-1s are length 2, ISO 639-2s are length 3
+    # ISO 639-1s are length 2, ISO 639-2s are length 3
+    |> validate_length(:language_code, min: 2, max: 3)
     |> validate_number(:view_count, greater_than: 0)
-    |> validate_inclusion(:year, 2007..2016) # The Wiki pagecount files are only for the years 2007-2016
+    # The Wiki pagecount files are only for the years 2007-2016
+    |> validate_inclusion(:year, 2007..2016)
     |> validate_inclusion(:month, 1..12)
     |> validate_inclusion(:day, 1..31)
   end
