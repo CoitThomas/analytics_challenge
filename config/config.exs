021aebbcdd1be23501ce6c11530bf78e3d60d211
Coit Scharringhausen <coit@simpli.fi>
2021-10-12 17:35:40 -0500

    Simplify config names

--- config/config.exs
+++ config/config.exs
@@ -3,13 +3,13 @@ import Config
 config :analytics_challenge,
   ecto_repos: [AnalyticsChallenge.Repo],
   loader: [
-    pagecounts_base_url: "https://dumps.wikimedia.org/other/pagecounts-raw",
-    db_insertion_batch_size: 10000
+    base_url: "https://dumps.wikimedia.org/other/pagecounts-raw",
+    batch_size: 10000
   ],
   writer: [
-    output_dir_name: "analytics",
-    output_file_descr: "top_ten_pagecounts_per_language",
-    output_file_type: "csv"
+    dir_name: "analytics",
+    file_descr: "top_ten_pagecounts_per_language",
+    file_type: "csv"
   ]
 
 config :analytics_challenge, AnalyticsChallenge.Repo,
