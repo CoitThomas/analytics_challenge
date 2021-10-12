ee05d169681f22d202263f456c867c0294bacc6d
Coit Scharringhausen <coit@simpli.fi>
2021-10-12 13:34:21 -0500

    Use support module and add output to csv file functionality

--- lib/analytics_challenge/import.ex
+++ lib/analytics_challenge/import.ex
@@ -2,6 +2,7 @@ defmodule AnalyticsChallenge.Import do
   @moduledoc """
   Responsible for fetching the pagecount data set from wikipedia.
   """
+  alias AnalyticsChallenge.NaiveDatetimeSupport
 
   @doc """
   Utilizes an HTTP client to access the pagecount historical data for a specified hour in the past.
@@ -13,11 +14,7 @@ defmodule AnalyticsChallenge.Import do
   end
 
   defp build_url(base_url, when_viewed) do
-    year = Integer.to_string(when_viewed.year)
-    month = to_padded_string(when_viewed.month)
-    day = to_padded_string(when_viewed.day)
-    hour = to_padded_string(when_viewed.hour)
-
+    {year, month, day, hour} = NaiveDatetimeSupport.parse_to_strings(when_viewed)
     "#{base_url}/#{year}/#{year}-#{month}/pagecounts-#{year}#{month}#{day}-#{hour}0000.gz"
   end
 
@@ -25,11 +22,5 @@ defmodule AnalyticsChallenge.Import do
     zipped_data
     |> :zlib.gunzip()
     |> String.split("\n", trim: true)
-  end
-
-  defp to_padded_string(integer) do
-    integer
-    |> Integer.to_string()
-    |> String.pad_leading(2, "0")
   end
 end
--- lib/analytics_challenge/persist.ex
+++ lib/analytics_challenge/persist.ex
@@ -2,21 +2,40 @@ defmodule AnalyticsChallenge.Persist do
   @moduledoc """
   X
   """
+  alias AnalyticsChallenge.NaiveDatetimeSupport
   alias AnalyticsChallenge.Pagecount
   alias AnalyticsChallenge.Repo
 
   @doc """
   X
   """
-  @spec to_postgres(list(map), NaiveDatetime.t(), pos_integer) :: :ok
+  @spec to_postgres(list(String.t()), NaiveDatetime.t(), pos_integer) :: :ok
   def to_postgres(raw_pagecounts, when_viewed, batch_size) do
     raw_pagecounts
-    |> prep_raw_pagecounts(when_viewed)
+    |> prep_for_postgres(when_viewed)
     |> Enum.chunk_every(batch_size)
     |> Enum.each(fn batch -> Repo.insert_all(Pagecount, batch) end)
   end
 
-  defp prep_raw_pagecounts(raw_pagecounts, when_viewed) do
+  @doc """
+  X
+  """
+  @spec to_csv(list(list(String.t() | pos_integer)), String.t()) :: :ok
+  def to_csv(pagecounts, path) do
+    :ok = ensure_path(path)
+    write_file(pagecounts, path)
+  end
+
+  @doc """
+  X
+  """
+  @spec build_filename(String.t(), NaiveDatetime.t(), String.t()) :: String.t()
+  def build_filename(descr, when_viewed, file_ext) do
+    {year, month, day, hour} = NaiveDatetimeSupport.parse_to_strings(when_viewed)
+    "#{descr}-#{year}#{month}#{day}-#{hour}0000.#{file_ext}"
+  end
+
+  defp prep_for_postgres(raw_pagecounts, when_viewed) do
     raw_pagecounts
     |> Enum.map(fn raw_pagecount -> String.split(raw_pagecount, " ") end)
     |> Enum.filter(&valid_raw_pagecount?/1)
@@ -42,5 +61,19 @@ defmodule AnalyticsChallenge.Persist do
       view_count: String.to_integer(view_count),
       when_viewed: when_viewed
     }
+  end
+
+  defp ensure_path(path) do
+    path
+    |> Path.dirname()
+    |> File.mkdir_p!()
+  end
+
+  defp write_file(content, path) do
+    file = File.open!(path, [:write, :utf8])
+
+    content
+    |> CSV.encode()
+    |> Enum.each(&IO.write(file, &1))
   end
 end
