31e1a08434b54e077dec92fbe2f20b4d848285e1
Coit Scharringhausen <coit@simpli.fi>
2021-10-11 23:29:57 -0500

    Update Loader moduledoc

--- lib/analytics_challenge/loader.ex
+++ lib/analytics_challenge/loader.ex
@@ -2,12 +2,14 @@ defmodule AnalyticsChallenge.Loader do
   @moduledoc """
   GenServer process that acts as a data importation driver program for the application.
 
-  When given a tuple with 4 strings representing the date and hour in the form: {YYYY, MM, DD, HH},
-  the worker will load the Wikipedia pagecounts data set for that particular date and hour into the
-  database.
+  When given a NaiveDatetime, the worker will load the historical Wikipedia pagecounts data set for
+  that particular date and hour into the database.
 
   NOTE: The range of dates and hours is fixed. It is only possible to fetch a data set for dates
-  and hours within the following range: {"2007", "12", "09", "18"} - {"2016", "08", "05", "12"}
+  and hours within the following range: ~N[2007-12-09 18:00:00] - ~N[2016-08-05 12:00:00].
+  Additionally, it should be understood that 'hour' is the smallest level of granularity. Including
+  anything other than zeros for the minutes and seconds of the NaiveDatetime will not yield any
+  results.
   """
   use GenServer
 
