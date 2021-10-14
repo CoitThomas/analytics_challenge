13e4c670c9aaf49fb76fd6eac4094a05f442d830
Coit Scharringhausen <coit@simpli.fi>
2021-10-14 18:53:33 -0500

    Adjust Repo configuration to help with potential timeout of long db transaction

--- config/dev.exs
+++ config/dev.exs
@@ -4,4 +4,6 @@ config :analytics_challenge, AnalyticsChallenge.Repo,
   database: "postgres_dev_1",
   username: "postgres",
   password: "postgres",
-  hostname: "localhost"
+  hostname: "localhost",
+  pool_size: 20,
+  queue_target: 5000
